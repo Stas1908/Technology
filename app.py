fd4db99339ec492438171a676ad98e6cf8b54f60
Stas1908 <mintianskyi.stanislav@chnu.edu.ua>
2025-05-22 19:07:00 +0300

    Оновлення файлів для коректного деплою на Render

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Лабораторна №1 через Render виконана успішно!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # бере порт із змінної середовища, 5000 — дефолт для локального запуску
+    app.run(host='0.0.0.0', port=port)
